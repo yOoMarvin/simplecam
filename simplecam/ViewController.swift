9bb607bd76a264333cbf887e1b54b1d4c46c5bf2
yOoMarvin <marvin.messenzehl@gmail.com>
2017-03-08 17:54:56 +0100

    Added the image add dialog with the image picker

--- simplecam/ViewController.swift
+++ simplecam/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MobileCoreServices
 
 class ViewController: UIViewController {
     
@@ -21,7 +22,9 @@ class ViewController: UIViewController {
             sheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: {
                 (action: UIAlertAction) in
                 
-                // TODO: pickerview
+                // create picker
+                let picker = self.preparePicker(srcType: .camera)
+                self.present(picker, animated: true, completion: nil)
             }))
 
         }
@@ -29,11 +32,28 @@ class ViewController: UIViewController {
         sheet.addAction(UIAlertAction(title: "Library", style: .default, handler: {
             (action: UIAlertAction) in
             
-            // TODO: pickerview
+            // create picker
+            let picker = self.preparePicker(srcType: .photoLibrary)
+            self.present(picker, animated: true, completion: nil)
         }))
         
         present(sheet, animated: true, completion: nil)
     }
+    
+    
+    
+    
+    private func preparePicker(srcType: UIImagePickerControllerSourceType) -> UIImagePickerController {
+    
+        let tmpPicker = UIImagePickerController()
+        tmpPicker.sourceType = srcType //comes from parameters
+        tmpPicker.mediaTypes = [kUTTypeImage as String]
+        tmpPicker.allowsEditing = true
+        tmpPicker.delegate = self
+        
+        return tmpPicker
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,3 +68,19 @@ class ViewController: UIViewController {
 
 }
 
+// MARK: Extensions
+
+extension ViewController:UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        // TODO: use picture, for example in storyboard
+        
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+
+extension ViewController: UINavigationControllerDelegate {
+    
+}
