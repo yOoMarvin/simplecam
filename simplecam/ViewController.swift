39e96caf5e505954183cf1dc2085bfcdc589ea1d
yOoMarvin <marvin.messenzehl@gmail.com>
2017-03-08 17:39:35 +0100

    Added sheet dialog for new pictures

--- simplecam/ViewController.swift
+++ simplecam/ViewController.swift
@@ -9,6 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBAction func newImageTapped(_ sender: Any) {
+        
+        let sheet = UIAlertController(title: "Please choose",
+                                      message: "Choose your source",
+                                      preferredStyle: .actionSheet)
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            sheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: {
+                (action: UIAlertAction) in
+                
+                // TODO: pickerview
+            }))
+
+        }
+        
+        sheet.addAction(UIAlertAction(title: "Library", style: .default, handler: {
+            (action: UIAlertAction) in
+            
+            // TODO: pickerview
+        }))
+        
+        present(sheet, animated: true, completion: nil)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
