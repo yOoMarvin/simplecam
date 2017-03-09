fe8bb36f8024e1cf6a3b4a69f31bc9e185a132a8
yOoMarvin <marvin.messenzehl@gmail.com>
2017-03-09 18:28:39 +0100

    Added files for camera overlay

---
+++ simplecam/CameraOverlayView.swift
@@ -0,0 +1,21 @@
+//
+//  CameraOverlayView.swift
+//  simplecam
+//
+//  Created by Marvin Messenzehl on 09.03.17.
+//  Copyright © 2017 Marvin Messenzehl. All rights reserved.
+//
+
+import UIKit
+
+class CameraOverlayView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
